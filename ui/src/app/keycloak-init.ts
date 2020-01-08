2c2e1ac84b36ec404537192cb2a92b47b6071970
Bartosz Sadel <bartosz.sadel@onwelo.com>
2020-01-08 21:43:57 +0100

    importing realm on image start + changed used realm

--- ui/src/app/keycloak-init.ts
+++ ui/src/app/keycloak-init.ts
@@ -4,7 +4,7 @@ export function initializer(keycloak: KeycloakService): () => Promise<any> {
   return (): Promise<any> => keycloak.init({
     config: {
       url: 'http://127.0.0.1:8081/auth/',
-      realm: 'master',
+      realm: 'onwelo-workshop',
       clientId: 'ui'
     },
     initOptions: {
